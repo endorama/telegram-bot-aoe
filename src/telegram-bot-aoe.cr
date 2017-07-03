e897de1db97519df8c4d82f1d7e5ee6af7f2172e
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-07-03 20:09:06 +0200

    initialize TauntCollection at bot startup

--- src/telegram-bot-aoe.cr
+++ src/telegram-bot-aoe.cr
@@ -4,7 +4,14 @@ require "./telegram-bot-aoe/*"
 
 module Telegram::Bot::Aoe
   def self.run
+    # puts Config.assets_path
+
     my_bot = Telegram::Bot::Aoe::MyBot.new
+
+    tc = TauntCollection.new
+    tc.load_for_locale(:en)
+    # puts tc.items.inspect
+
     my_bot.polling
   end
 end
