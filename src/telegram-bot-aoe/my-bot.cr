6919351638e94f8bc9697bd2a05d87a3f6757747
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-07-03 20:37:09 +0200

    send proper markdown to telegram

--- src/telegram-bot-aoe/my-bot.cr
+++ src/telegram-bot-aoe/my-bot.cr
@@ -11,15 +11,15 @@ module Telegram::Bot::Aoe
       @taunts.load_for_locale :en
 
       cmd "start" do |msg|
-        send msg, HELP_TEXT
+        send_markdown_message msg.chat.id, Telegram::Bot::Aoe::HELP_TEXT
       end
 
       cmd "help" do |msg|
-        send msg, HELP_TEXT
+        send_markdown_message msg.chat.id, Telegram::Bot::Aoe::HELP_TEXT
       end
 
       cmd "list" do |msg|
-        send msg, @taunts.list
+        send_markdown_message msg.chat.id, @taunts.list
       end
     end
 
@@ -29,16 +29,20 @@ module Telegram::Bot::Aoe
       # puts message.text
       if text = message.text
         if @taunts.includes? text
-          send_taunt message, text
+          send_taunt message.chat.id, text
         else
           super message
         end
       end
     end
 
-    private def send_taunt(message, text)
+    private def send_taunt(chat_id, text)
       taunt = @taunts.get(text)
-      send_audio(message.chat.id, File.new(taunt.path), nil, nil, taunt.title ) if taunt
+      send_audio(chat_id, File.new(taunt.path), nil, nil, taunt.title ) if taunt
+    end
+
+    private def send_markdown_message(chat_id, text)
+      send_message chat_id, text, "Markdown"
     end
   end
 end
