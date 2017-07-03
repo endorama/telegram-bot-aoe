b09b6ecca39624188beda6deaec5833c1a55c7b5
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-07-03 20:14:08 +0200

    add dotenv

--- src/telegram-bot-aoe.cr
+++ src/telegram-bot-aoe.cr
@@ -1,16 +1,17 @@
 require "telegram_bot"
+require "dotenv"
 
 require "./telegram-bot-aoe/*"
 
 module Telegram::Bot::Aoe
   def self.run
+    Dotenv.load
     # puts Config.assets_path
 
     my_bot = Telegram::Bot::Aoe::MyBot.new
 
     tc = TauntCollection.new
     tc.load_for_locale(:en)
-    # puts tc.items.inspect
 
     my_bot.polling
   end
