07778a021a15d29335738abacfc4606ee05f6d78
Edoardo Tenani <edoardo.tenani@gmail.com>
2017-07-03 20:36:52 +0200

    move bot help text

---
+++ src/telegram-bot-aoe/help-text.cr
@@ -0,0 +1,25 @@
+module Telegram::Bot::Aoe
+  HELP_TEXT = <<-EOS
+"Now we know his name: Sir William Wallace, the Hammer of the English."
+
+Welcome to this AoE2 inspired bot. Send him chat taunts, and receive audio
+back. Simple as that.
+
+Commands:
+
+- /list: get a list of available taunts
+- /help to show this text.
+
+To stop receiving messages from this bot, remove it from the group.
+
+*Privacy Note:* This bot reads *all* messages send in the group chat where is
+present. :)
+Not that it saves the messages, but they could be logged for debug purposes.
+
+Enjoy!
+
+Disclaimer:
+Age of Empires 2 HD © Microsoft Corporation.
+WilliamWallaceBot was created under Microsoft's "Game Content Usage Rules" using assets from Age of Empires 2 HD, and it is not endorsed by or affiliated with Microsoft.
+EOS
+end
